2a1af2ca244044655b9d912e2d2a2c233a988dd3
kashyap-sanchit <sanchit.kashyap@walkingtree.tech>
2025-02-21 15:22:11 +0530

    Added Ingestion Service

---
+++ app/services/ingestion.py
@@ -0,0 +1,36 @@
+import uuid
+from app.db.qdrant import qdrant_client, COLLECTION_NAME
+from app.utils.embedding_utils import EmbeddingUtils
+from app.utils.document_processor import extract_data_from_document
+from qdrant_client.models import PointStruct
+from app.core.logger import logger
+
+
+class Ingestion(EmbeddingUtils):
+
+    def __init__(self):
+        super().__init__()
+
+    def upload_document(self, title, content):
+
+        try:
+
+            points = []
+            text_data = extract_data_from_document(content)
+
+            for text in text_data:
+
+                embedding = self.generate_embedding(str(text))
+                points.append(
+                    PointStruct(
+                        id=str(uuid.uuid4()),
+                        vector=embedding,
+                        payload={"id": str(uuid.uuid4()), "text": text, "title": title}  
+                    )
+                )
+
+            qdrant_client.upsert(collection_name=COLLECTION_NAME, points=points)
+        
+        except Exception as e:
+            logger.error(f"Error while uploading document: {str(e)}")
+            raise ValueError(f"Error while uploading document: {str(e)}")
